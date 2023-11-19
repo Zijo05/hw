5f5fca00a75f943397514c7ffff18982451e05aa
Deisby <129885083+Zijo05@users.noreply.github.com>
2023-11-19 21:38:35 +0100

    Euler Problems

--- euler_problems/problem_9.cpp
+++ euler_problems/problem_9.cpp
@@ -7,22 +7,17 @@ using namespace std;
 
 int main()
 {
-    int n = 1000;
-    int a, b;
-    double c = 0;
+    for(int a = 1; a < 1000; a++){
+        for(int b = a+1; b < 1000; b++){
+            double c = sqrt( pow(a, 2) + pow(b, 2) );
+            if(a + b + c == 1000) {
+                cout << "Pythagorean triplet is: " << a << "^2 + " << b << "^2 = " << c << "^2" << endl;
+                cout << "Product abc is: " << a * b * c;
+                return 0;
 
-    for(a = 1; a < n; a++){
-        for(b = a+1; b < n; b++){
-            c = sqrt( pow(a, 2) + pow(b, 2) );
-            if(a + b + c == 1000)
-                break;
+            }
         }
-        if(a + b + c == 1000)
-            break;
     }
-
-    cout << "Pythagorean triplet is: " << a << "^2 + " << b << "^2 = " << c << "^2" << endl;
-    cout << "Product abc is: " << a * b * c;
 
     return 0;
 }
