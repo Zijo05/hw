6bb1a4eda0661e2537d9887d76692e12a0f9c9e2
Deisby <129885083+Zijo05@users.noreply.github.com>
2023-11-19 21:39:16 +0100

    Euler Problems

--- euler_problems/problem_9.cpp
+++ euler_problems/problem_9.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
     for(int a = 1; a < 1000; a++){
-        for(int b = a+1; b < 1000; b++){
+        for(int b = a + 1; b < 1000; b++){
             double c = sqrt( pow(a, 2) + pow(b, 2) );
             if(a + b + c == 1000) {
                 cout << "Pythagorean triplet is: " << a << "^2 + " << b << "^2 = " << c << "^2" << endl;
